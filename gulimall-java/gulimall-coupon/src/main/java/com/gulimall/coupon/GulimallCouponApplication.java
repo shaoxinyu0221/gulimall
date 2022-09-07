58493d7c22011802f4b8fa38b2050c4f5e57550b
邵鑫雨 <2424590457@qq.com>
2022-09-07 19:52:36 +0800

    添加注解

--- gulimall-java/gulimall-coupon/src/main/java/com/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-java/gulimall-coupon/src/main/java/com/gulimall/coupon/GulimallCouponApplication.java
@@ -1,9 +1,15 @@
 package com.gulimall.coupon;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableRabbit
+@EnableDiscoveryClient
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
